f47a3960a8b73b2777c9dfb07b79646e7d6b4660
Sankha Narayan Guria <sankha93@gmail.com>
2014-12-11 12:00:39 +0530

    convert code to a library

---
+++ tests/lib.rs
@@ -0,0 +1,23 @@
+extern crate num_rs;
+use num_rs::matrix;
+
+#[test]
+fn test_basic_matrix() {
+  let mut m = matrix::Matrix::new(2, 2, 0.0);
+  // test is rows and columns are as expected
+  assert_eq!(m.num_rows(), 2);
+  assert_eq!(m.num_cols(), 2);
+
+  // test for correctness of values
+  for i in range(0, m.num_rows()) {
+    for j in range(0, m.num_cols()) {
+      m.set(i, j, (i + j) as f64);
+    }
+  }
+
+  for i in range(0, m.num_rows()) {
+    for j in range(0, m.num_cols()) {
+      assert_eq!(m.get(i, j), (i + j) as f64);
+    }
+  }
+}
