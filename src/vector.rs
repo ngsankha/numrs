ec68423e3a89d09f2792e8701db07f08b3c5387d
Sankha Narayan Guria <sankha93@gmail.com>
2015-09-21 23:31:06 +0530

    SIMD elementwise arithmetic ops

--- src/vector.rs
+++ src/vector.rs
@@ -2,7 +2,7 @@ extern crate num;
 extern crate simd;
 
 use self::num::traits::Float;
-use std::ops::{Index, Add};
+use std::ops::{Index, Add, Sub, Mul, Div, Neg};
 use self::simd::f32x4;
 
 pub struct Vector<T: Float> {
@@ -122,6 +122,169 @@ impl Add<Vector<f32>> for Vector<f32> {
       Ok(Vector::<f32> { data: new_vec })
     } else {
       Err("Vectors are not conformable for addition.".to_string())
+    }
+  }
+}
+
+impl Neg for Vector<f32> {
+  type Output = Vector<f32>;
+
+  fn neg(self) -> Vector<f32> {
+    let mut new_vec = Vec::new();
+    let data = self.data.as_slice();
+    for i in (0..self.data.len()).step_by(4) {
+      let mut reg_len = 4;
+      let reg: f32x4;
+      if self.data.len() - i < 4 {
+        let (mut x1, mut x2, mut x3) = (0.0_f32, 0.0_f32, 0.0_f32);
+        reg_len = self.data.len() - i;
+        for j in i..self.data.len() {
+          let diff = self.data.len() - j;
+          match diff {
+            1 => { x1 = data[j] },
+            2 => { x2 = data[j] },
+            3 => { x3 = data[j] },
+            _ => { unreachable!() }
+          }
+        }
+        reg = f32x4::new(x1, x2, x3, 0.0_f32);
+      } else {
+        reg = f32x4::load(data, i);
+      }
+      let res = -reg;
+      for j in 0..reg_len {
+        new_vec.push(res.extract(j as u32));
+      }
+    }
+    Vector::<f32> { data: new_vec }
+  }
+}
+
+impl Sub<Vector<f32>> for Vector<f32> {
+  type Output = Result<Vector<f32>, String>;
+
+  fn sub(self, rhs: Vector<f32>) -> Result<Vector<f32>, String> {
+    if self.data.len() == rhs.data.len() {
+      let mut new_vec = Vec::new();
+      let lhs_data = self.data.as_slice();
+      let rhs_data = rhs.data.as_slice();
+      for i in (0..self.data.len()).step_by(4) {
+        let mut reg_len = 4;
+        let reg1: f32x4;
+        let reg2: f32x4;
+        if self.data.len() - i < 4 {
+          let (mut x1, mut x2, mut x3) = (0.0_f32, 0.0_f32, 0.0_f32);
+          let (mut y1, mut y2, mut y3) = (0.0_f32, 0.0_f32, 0.0_f32);
+          reg_len = self.data.len() - i;
+          for j in i..self.data.len() {
+            let diff = self.data.len() - j;
+            match diff {
+              1 => { x1 = lhs_data[j]; y1 = rhs_data[j] },
+              2 => { x2 = lhs_data[j]; y2 = rhs_data[j] },
+              3 => { x3 = lhs_data[j]; y3 = rhs_data[j] },
+              _ => { unreachable!() }
+            }
+          }
+          reg1 = f32x4::new(x1, x2, x3, 0.0_f32);
+          reg2 = f32x4::new(y1, y2, y3, 0.0_f32);
+        } else {
+          reg1 = f32x4::load(lhs_data, i);
+          reg2 = f32x4::load(rhs_data, i);
+        }
+        let res = reg1 - reg2;
+        for j in 0..reg_len {
+          new_vec.push(res.extract(j as u32));
+        }
+      }
+      Ok(Vector::<f32> { data: new_vec })
+    } else {
+      Err("Vectors are not conformable for subtraction.".to_string())
+    }
+  }
+}
+
+impl Mul<Vector<f32>> for Vector<f32> {
+  type Output = Result<Vector<f32>, String>;
+
+  fn mul(self, rhs: Vector<f32>) -> Result<Vector<f32>, String> {
+    if self.data.len() == rhs.data.len() {
+      let mut new_vec = Vec::new();
+      let lhs_data = self.data.as_slice();
+      let rhs_data = rhs.data.as_slice();
+      for i in (0..self.data.len()).step_by(4) {
+        let mut reg_len = 4;
+        let reg1: f32x4;
+        let reg2: f32x4;
+        if self.data.len() - i < 4 {
+          let (mut x1, mut x2, mut x3) = (0.0_f32, 0.0_f32, 0.0_f32);
+          let (mut y1, mut y2, mut y3) = (0.0_f32, 0.0_f32, 0.0_f32);
+          reg_len = self.data.len() - i;
+          for j in i..self.data.len() {
+            let diff = self.data.len() - j;
+            match diff {
+              1 => { x1 = lhs_data[j]; y1 = rhs_data[j] },
+              2 => { x2 = lhs_data[j]; y2 = rhs_data[j] },
+              3 => { x3 = lhs_data[j]; y3 = rhs_data[j] },
+              _ => { unreachable!() }
+            }
+          }
+          reg1 = f32x4::new(x1, x2, x3, 0.0_f32);
+          reg2 = f32x4::new(y1, y2, y3, 0.0_f32);
+        } else {
+          reg1 = f32x4::load(lhs_data, i);
+          reg2 = f32x4::load(rhs_data, i);
+        }
+        let res = reg1 * reg2;
+        for j in 0..reg_len {
+          new_vec.push(res.extract(j as u32));
+        }
+      }
+      Ok(Vector::<f32> { data: new_vec })
+    } else {
+      Err("Vectors are not conformable for multiplication.".to_string())
+    }
+  }
+}
+
+impl Div<Vector<f32>> for Vector<f32> {
+  type Output = Result<Vector<f32>, String>;
+
+  fn div(self, rhs: Vector<f32>) -> Result<Vector<f32>, String> {
+    if self.data.len() == rhs.data.len() {
+      let mut new_vec = Vec::new();
+      let lhs_data = self.data.as_slice();
+      let rhs_data = rhs.data.as_slice();
+      for i in (0..self.data.len()).step_by(4) {
+        let mut reg_len = 4;
+        let reg1: f32x4;
+        let reg2: f32x4;
+        if self.data.len() - i < 4 {
+          let (mut x1, mut x2, mut x3) = (0.0_f32, 0.0_f32, 0.0_f32);
+          let (mut y1, mut y2, mut y3) = (0.0_f32, 0.0_f32, 0.0_f32);
+          reg_len = self.data.len() - i;
+          for j in i..self.data.len() {
+            let diff = self.data.len() - j;
+            match diff {
+              1 => { x1 = lhs_data[j]; y1 = rhs_data[j] },
+              2 => { x2 = lhs_data[j]; y2 = rhs_data[j] },
+              3 => { x3 = lhs_data[j]; y3 = rhs_data[j] },
+              _ => { unreachable!() }
+            }
+          }
+          reg1 = f32x4::new(x1, x2, x3, 0.0_f32);
+          reg2 = f32x4::new(y1, y2, y3, 0.0_f32);
+        } else {
+          reg1 = f32x4::load(lhs_data, i);
+          reg2 = f32x4::load(rhs_data, i);
+        }
+        let res = reg1 / reg2;
+        for j in 0..reg_len {
+          new_vec.push(res.extract(j as u32));
+        }
+      }
+      Ok(Vector::<f32> { data: new_vec })
+    } else {
+      Err("Vectors are not conformable for division.".to_string())
     }
   }
 }
--- tests/vector.rs
+++ tests/vector.rs
@@ -32,3 +32,60 @@ fn test_vector_add() {
   assert_eq!(res[4], 10.0);
   assert_eq!(res.len(), 5);
 }
+
+#[test]
+fn test_vector_sub() {
+  let arr1 = [1.0, 2.0, 3.0, 4.0, 5.0];
+  let arr2 = [1.0, 1.0, 1.0, 1.0, 1.0];
+  let v1 = Vector::<f32>::new(&arr1);
+  let v2 = Vector::<f32>::new(&arr2);
+  let res = (v1 - v2).unwrap();
+  assert_eq!(res[0], 0.0);
+  assert_eq!(res[1], 1.0);
+  assert_eq!(res[2], 2.0);
+  assert_eq!(res[3], 3.0);
+  assert_eq!(res[4], 4.0);
+  assert_eq!(res.len(), 5);
+}
+
+#[test]
+fn test_vector_mul() {
+  let arr = [1.0, 2.0, 3.0, 4.0, 5.0];
+  let v1 = Vector::<f32>::new(&arr);
+  let v2 = Vector::<f32>::new(&arr);
+  let res = (v1 * v2).unwrap();
+  assert_eq!(res[0], 1.0);
+  assert_eq!(res[1], 4.0);
+  assert_eq!(res[2], 9.0);
+  assert_eq!(res[3], 16.0);
+  assert_eq!(res[4], 25.0);
+  assert_eq!(res.len(), 5);
+}
+
+#[test]
+fn test_vector_div() {
+  let arr1 = [2.0, 4.0, 6.0, 8.0, 10.0];
+  let arr2 = [2.0, 2.0, 2.0, 2.0, 2.0];
+  let v1 = Vector::<f32>::new(&arr1);
+  let v2 = Vector::<f32>::new(&arr2);
+  let res = (v1 / v2).unwrap();
+  assert_eq!(res[0], 1.0);
+  assert_eq!(res[1], 2.0);
+  assert_eq!(res[2], 3.0);
+  assert_eq!(res[3], 4.0);
+  assert_eq!(res[4], 5.0);
+  assert_eq!(res.len(), 5);
+}
+
+#[test]
+fn test_vector_neg() {
+  let arr = [1.0, 2.0, 3.0, 4.0, 5.0];
+  let v = Vector::<f32>::new(&arr);
+  let res = -v.clone();
+  assert_eq!(res[0], -v[0]);
+  assert_eq!(res[1], -v[1]);
+  assert_eq!(res[2], -v[2]);
+  assert_eq!(res[3], -v[3]);
+  assert_eq!(res[4], -v[4]);
+  assert_eq!(res.len(), 5);
+}
