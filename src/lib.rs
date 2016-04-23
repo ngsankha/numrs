719ccb18115597cd3bc9c489fd6db51e5d0adba2
Sankha Narayan Guria <sankha93@gmail.com>
2016-04-24 00:39:49 +0530

    Revert "SIMD Vectors"

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,2 @@
-#![feature(step_by, convert)]
-
-/// A Vector type for floating point numbers.
-///
-/// The `Vector`s can be composed of `f32` or `f64` elements.
-pub mod vector;
-
-/// A 2D Matrix type composed of `f64` elements.
+/// A 2D Matrix type composed of f64 elements.
 pub mod matrix;
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,3 @@
-use std::ops::{Index, Add, Sub, Mul, Neg};
-
 /// A 2D Matrix type composed of `f64` elements.
 ///
 /// The Matrix type supports simple arithmetic operations from the start and
@@ -22,6 +20,8 @@ use std::ops::{Index, Add, Sub, Mul, Neg};
 /// res = m1.clone() * m2.clone(); // matrix product of 2 matrices
 /// res = m2.clone() * 5.0; // scalar product of a matrix
 /// ```
+
+use std::ops::{Index, Add, Sub, Mul, Neg};
 
 pub struct Matrix {
   rows: usize,
